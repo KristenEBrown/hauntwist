175ebfb665da954394e78613a37a8405ed543831
unknown <laurenprinn98@gmail.com>
2018-10-20 17:58:31 -0400

    kldjfkajs

--- app/src/main/java/edu/gatech/hauntwist/RoomItem.java
+++ app/src/main/java/edu/gatech/hauntwist/RoomItem.java
@@ -1,4 +1,19 @@
 package edu.gatech.hauntwist;
+public enum RoomItem {
+    DEADBODY(
 
-public class RoomItem {
+
+
+    public RoomItem(String name, String clue) {
+        this.name = name;
+        this.clue = clue;
+    }
+
+    public String toString() {
+        return "You've picked up a " + this.name + ". " + this.clue;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
