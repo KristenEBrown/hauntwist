3d34155516d5808e0bcfad336ab200426b7c2ced
Jacob Chambers <wchambers8@gatech.edu>
2018-10-20 14:41:50 -0400

    Added structure of UI elements for display

--- app/src/main/java/edu/gatech/hauntwist/DisplayActivity.java
+++ app/src/main/java/edu/gatech/hauntwist/DisplayActivity.java
@@ -2,12 +2,49 @@ package edu.gatech.hauntwist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    private Button forwardButton;
+    private Button leftButton;
+    private Button rightButton;
+
+    private View textBackground;
+    private TextView msgText;
+
+    private ImageView display;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        setAttributes();
+
+        String test = "You wake up in a dimly lit room. Looking around, " +
+                "you see that the room is mostly empty, save for a few " +
+                "scattered items.";
+
+        msgText.setText(test);
+
+
+
+
+    }
+
+    private void setAttributes() {
+        forwardButton = findViewById(R.id.forwardBttn);
+        leftButton = findViewById(R.id.leftBttn);
+        rightButton = findViewById(R.id.rightBttn);
+
+        textBackground = findViewById(R.id.textWrapper);
+        msgText = findViewById(R.id.messageText);
+
+        display = findViewById(R.id.displayView);
     }
 }
--- app/src/main/java/edu/gatech/hauntwist/SplashActivity.java
+++ app/src/main/java/edu/gatech/hauntwist/SplashActivity.java
@@ -1,7 +1,9 @@
 package edu.gatech.hauntwist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -36,7 +38,16 @@ public class SplashActivity extends AppCompatActivity {
                 .setDuration(buttonAnimationDuration)
                 .setListener(null);
 
-        enterButton.setOnClickListener();
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enter();
+            }});
 
     }
+
+    private void enter() {
+            startActivity(new Intent(SplashActivity.this, DisplayActivity.class));
+        }
+
 }
