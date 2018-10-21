92ab0f48e08ca72a26c438bb6cdd7e0411c7062f
unknown <laurenprinn98@gmail.com>
2018-10-21 01:49:42 -0400

    here

--- app/src/main/java/edu/gatech/hauntwist/RoomItem.java
+++ app/src/main/java/edu/gatech/hauntwist/RoomItem.java
@@ -7,10 +7,10 @@ public enum RoomItem {
     LOVELETTER("love letter addressed to you from Madeline", "You read that she is hoping to avenge your death. It sounds like she's apologizing for something"),
     DIARY("diary", "You flip to the first page and can tell that it is Norris's diary. He seems to be writing about feeling scared of Madeline. " +
             "Why would he be afraid of her?"),
-    PILLBOTTLE("pill bottle", "The bottle contains arsenic! It looks like there's a splash of " +
+    PILLBOTTLE("pill bottle", "The bottle contains cyanide! It looks like there's a splash of " +
             "spaghetti sauce on it the side of the bottle. That's odd that something so deadly would be near food."),
     PEN("pen and paper", "You quickly try to write a note warning Norris, but oh no! The pen is out of ink"),
-    PILLBOTTLEEMPTY("empty pill bottle", "It is the same bottle as before, but now it is empty! What could Madeline have done with the rest of the arsenic?");
+    PILLBOTTLEEMPTY("empty pill bottle", "It is the same bottle as before, but now it is empty! What could Madeline have done with the rest of the cyanide?");
 
 
     private String name;
